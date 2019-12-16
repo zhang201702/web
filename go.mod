309808f40efaa1031d98632e8f6f94f04796f9a8
zbfwater@163.com <zbfwater@163.com>
2019-12-16 15:11:32 +0800

    create

---
+++ go.mod
@@ -0,0 +1,5 @@
+module web
+
+go 1.13
+
+require github.com/zhang201702/zhang v1.0.6 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/zhang201702/zhang"
+)
+
+func main()  {
+
+	s := zhang.Default()
+	s.Run()
+}
